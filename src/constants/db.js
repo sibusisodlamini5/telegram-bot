0e2c61228b73133c5f6d5149e73ff025d35b0e1c
Sibusiso <cbucsodlamini@gmail.com>
2024-10-10 09:10:31 +0200

    daw

--- src/constants/db.js
+++ src/constants/db.js
@@ -2,19 +2,19 @@ export function getData() {
     return [
         {
             title: "Raffle Ticket",
-            price: 5,
+            price: 2.5,
             Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMK6IFvRCbeomD2wijBnlYyqYx5AH_An65EQ&s',
             id: 1,
         },   
         {
             title: "10 Raffle Tickets",
-            price: 50,
+            price: 25,
             Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMK6IFvRCbeomD2wijBnlYyqYx5AH_An65EQ&s',
             id: 2,
         },
         {
             title: "100 Raffle Tickets",
-            price: 500,
+            price: 250,
             Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMK6IFvRCbeomD2wijBnlYyqYx5AH_An65EQ&s',
             id: 3,
         },         
